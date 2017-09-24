409b0018863e309b0df92e2b4d9ab7bb8da1a9d7
vikesh <vikesh@abinn.com>
2017-09-24 17:21:13 +0530

    sonar scanner

--- src/main/java/abi/App.java
+++ src/main/java/abi/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello i--World!bharath!" );
+        System.out.println( "Hello i--World! sonar scan bharath!" );
     }
 }
